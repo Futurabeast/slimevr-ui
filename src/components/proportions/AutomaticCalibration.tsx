dcc00daf5dd28826489e91b0f5fa684dc4260d95
Butterscotch! <bscotchvanilla@gmail.com>
2022-04-28 15:57:21 -0400

    Add basic AutoBone interface

--- src/components/proportions/AutomaticCalibration.tsx
+++ src/components/proportions/AutomaticCalibration.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { AutoBoneEpochT, AutoBoneProcessRequestT, AutoBoneProcessStatusT, AutoBoneProcessType, RpcMessage } from "solarxr-protocol";
+import { useWebsocketAPI } from "../../hooks/websocket-api";
 import { Button } from "../commons/Button";
 import { AppModal } from "../Modal";
 
@@ -6,7 +8,64 @@ import { AppModal } from "../Modal";
 export function AutomaticCalibration() {
 
     const [isOpen, setOpen] = useState(false);
-    
+    const [hasRecording, setHasRecording] = useState(false);
+    const [hasCalibration, setHasCalibration] = useState(false);
+
+    const { useRPCPacket, sendRPCPacket } = useWebsocketAPI();
+
+    const startProcess = (processType: AutoBoneProcessType) => {
+        const processRequest = new AutoBoneProcessRequestT();
+        processRequest.processType = processType;
+        
+        sendRPCPacket(RpcMessage.AutoBoneProcessRequest, processRequest)
+    }
+
+    const startRecording = () => {
+        setHasRecording(false);
+        startProcess(AutoBoneProcessType.RECORD);
+    }
+
+    const startProcessing = () => {
+        setHasCalibration(false);
+        startProcess(AutoBoneProcessType.PROCESS);
+    }
+
+    useRPCPacket(RpcMessage.AutoBoneProcessStatus, (data: AutoBoneProcessStatusT) => {
+        if (data.processType) {
+            if (data.message) {
+                console.log(data.processType, data.message);
+            }
+
+            if (data.completed) {
+                console.log("Process ", data.processType, " has completed");
+
+                switch (data.processType) {
+                    case AutoBoneProcessType.RECORD:
+                        setHasRecording(data.success);
+                        break;
+
+                    case AutoBoneProcessType.PROCESS:
+                        setHasCalibration(data.success);
+                        break;
+                }
+            }
+        }
+    })
+
+    useRPCPacket(RpcMessage.AutoBoneEpoch, (data: AutoBoneEpochT) => {
+        if (data.currentEpoch && data.totalEpochs) {
+            if (data.epochError) {
+                // Probably not necessary to show to the user
+                console.log("Epoch ", data.currentEpoch, "/", data.totalEpochs, " (Error ", data.epochError, ")");
+            }
+
+            // Progress bar?
+        }
+
+        if (data.adjustedSkeletonParts) {
+            // Display measurements
+        }
+    })
 
     return (
         <>
@@ -17,12 +76,14 @@ export function AutomaticCalibration() {
                 onRequestClose={() => setOpen(false)}
             >
                 <>
-                    <div className="flex w-full justify-center">
-                        <Button variant="primary">Start Calibration</Button>
+                    <div className="flex w-full justify-center gap-3">
+                    <Button variant="primary" onClick={startRecording}>Start Recording</Button>
+                    <Button variant="primary" onClick={() => startProcess(AutoBoneProcessType.SAVE)} disabled={!hasRecording}>Save Recording</Button>
+                    <Button variant="primary" onClick={startProcessing}>Start Calibration</Button>
                     </div>
                     <div className="flex w-full justify-between mt-5">
                         <Button variant="primary" onClick={() => setOpen(false)}>Close</Button>
-                        <Button variant="primary" disabled>Apply values</Button>
+                        <Button variant="primary" onClick={() => startProcess(AutoBoneProcessType.APPLY)} disabled={!hasCalibration}>Apply values</Button>
                     </div>
                 </>
             </AppModal>
